2522949f31e6d97d9924659fbe20002836dcfc91
Souhail El Kamel <souhail.elkamel@cvn-solutions.com>
2025-05-23 13:17:41 +0200

    Implementing Cart management

---
+++ back/PROJECT/PROJECT-API/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PROJECT.Domain.Entities;
+using PROJECT.Infrastructure.Data;
+using System;
+using System.Security.Claims;
+
+namespace PROJECT_API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("cart")]
+    public class CartController : ControllerBase
+    {
+        private readonly ProjectDbContext _context;
+
+        public CartController(ProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _context.Users.Include(u => u.Cart).FirstOrDefaultAsync(u => u.email == email);
+            if (user == null) return NotFound();
+
+            return Ok(user.Cart);
+        }
+
+        [HttpPost("{productId}")]
+        public async Task<IActionResult> AddToCart(int productId)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _context.Users.Include(u => u.Cart).FirstOrDefaultAsync(u => u.email == email);
+            var product = await _context.Products.FindAsync(productId);
+
+            if (user == null || product == null) return NotFound();
+
+            user.Cart.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok("Added to cart");
+        }
+    }
+}
